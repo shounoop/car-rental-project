8844e91df4ab541230d2516b23595a2315df53f5
HMT <tuan.hm@tinasoft.com.vn>
2024-02-20 23:58:02 +0700

    be: adding cors | fe: calling signup api

--- car-rental-angular/src/app/app.module.ts
+++ car-rental-angular/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
-import { LoginComponent } from './auth/components/login/login.component'
-import { SignupComponent } from './auth/components/signup/signup.component'
-
 // NG ZORRO IMPORTS
 import { NzSpinModule } from 'ng-zorro-antd/spin'
 import { NzFormModule } from 'ng-zorro-antd/form'
 import { NzButtonModule } from 'ng-zorro-antd/button'
 import { NzInputModule } from 'ng-zorro-antd/input'
 import { NzLayoutModule } from 'ng-zorro-antd/layout'
+
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { LoginComponent } from './auth/components/login/login.component'
+import { SignupComponent } from './auth/components/signup/signup.component'
 import { ReactiveFormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http'
+import { NzMessageModule } from 'ng-zorro-antd/message'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, SignupComponent],
@@ -24,7 +27,10 @@ import { ReactiveFormsModule } from '@angular/forms'
     NzButtonModule,
     NzInputModule,
     NzLayoutModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    NzMessageModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- car-rental-angular/src/app/auth/components/services/auth/auth.service.ts
+++ car-rental-angular/src/app/auth/components/services/auth/auth.service.ts
@@ -1,8 +1,16 @@
+import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
+
+const BASE_URL = ['http://localhost:8080']
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  register(signupRequest: any): Observable<any> {
+    return this.http.post(`${BASE_URL}/api/auth/signup`, signupRequest)
+  }
 }
