61ba4aaaede61c72e9a815582ff3b54272b53073
HMT <tuan.hm@tinasoft.com.vn>
2024-02-20 14:03:32 +0700

    initialize be project with spring boot and connted to mysql database

---
+++ car-rental-spring/src/main/java/com/shounoop/carrentalspring/CarRentalSpringApplication.java
@@ -0,0 +1,13 @@
+package com.shounoop.carrentalspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalSpringApplication.class, args);
+	}
+
+}
---
+++ car-rental-spring/src/test/java/com/shounoop/carrentalspring/CarRentalSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shounoop.carrentalspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
