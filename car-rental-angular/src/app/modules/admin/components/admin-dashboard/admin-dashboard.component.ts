526069acbd8339095b9901db8de997c9c82b0329
HMT <tuan.hm@tinasoft.com.vn>
2024-02-25 22:53:06 +0700

    fe: calling get all cars api and rendering it to dashboard

--- car-rental-angular/src/app/modules/admin/components/admin-dashboard/admin-dashboard.component.ts
+++ car-rental-angular/src/app/modules/admin/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core'
+import { AdminService } from '../../services/admin.service'
 
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.scss'
 })
-export class AdminDashboardComponent {}
+export class AdminDashboardComponent {
+  cars: any[] = []
+
+  constructor(private adminService: AdminService) {}
+
+  ngOnInit() {
+    this.getAllCars()
+  }
+
+  getAllCars() {
+    this.adminService.getAllCars().subscribe(res => {
+      res.forEach((car: any) => {
+        car.processedImg = `data:image/jpeg;base64,${car.returnedImage}`
+        this.cars.push(car)
+      })
+    })
+  }
+}
